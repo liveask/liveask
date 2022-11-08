f340664c246f9e2ecbbdf3ae78448d484c434c45
extrawurst <mail@rusticorn.com>
2022-11-08 10:49:18 +0100

    aws based backend (WIP)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,15 @@ mod app;
 mod handle;
 mod mail;
 
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::{
+    model::{
+        AttributeDefinition, KeySchemaElement, KeyType, ProvisionedThroughput, ScalarAttributeType,
+    },
+    Credentials, Endpoint,
+};
 use axum::{
+    http::Uri,
     routing::{get, post},
     Extension, Router,
 };
@@ -23,6 +31,60 @@ fn setup_cors() -> CorsLayer {
     CorsLayer::very_permissive()
 }
 
+async fn test_aws() {
+    use aws_sdk_dynamodb::Client;
+
+    let region_provider = RegionProviderChain::default_provider().or_else("us-west-1");
+    let config = aws_config::from_env()
+        .region(region_provider)
+        .credentials_provider(Credentials::new("aid", "sid", None, None, "local"))
+        .endpoint_resolver(Endpoint::immutable(Uri::from_static(
+            "http://localhost:8000",
+        )))
+        .load()
+        .await;
+
+    let client = Client::new(&config);
+
+    let resp = client.list_tables().send().await.unwrap();
+    let names = resp.table_names().unwrap_or_default();
+
+    tracing::trace!("tables: {}", names.join(","));
+
+    if !names.contains(&"liveask".into()) {
+        tracing::info!("table not found, creating now");
+        create_table(&client, "liveask".into(), "key".into()).await;
+    }
+}
+
+//TODO: error handling
+async fn create_table(client: &aws_sdk_dynamodb::Client, table_name: String, key_name: String) {
+    let ad = AttributeDefinition::builder()
+        .attribute_name(&key_name)
+        .attribute_type(ScalarAttributeType::S)
+        .build();
+
+    let ks = KeySchemaElement::builder()
+        .attribute_name(&key_name)
+        .key_type(KeyType::Hash)
+        .build();
+
+    let pt = ProvisionedThroughput::builder()
+        .read_capacity_units(5)
+        .write_capacity_units(5)
+        .build();
+
+    client
+        .create_table()
+        .table_name(table_name)
+        .attribute_definitions(ad)
+        .key_schema(ks)
+        .provisioned_throughput(pt)
+        .send()
+        .await
+        .unwrap();
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
@@ -31,6 +93,8 @@ async fn main() {
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
+
+    test_aws().await;
 
     let app = App::new();
 
