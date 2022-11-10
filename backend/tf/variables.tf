cd7588af8bec16cdf0c2c963371f3c0ffefa758f
extrawurst <mail@rusticorn.com>
2022-11-10 22:35:03 +0100

    terraform wip

---
+++ backend/tf/variables.tf
@@ -0,0 +1,35 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "aws_id" {}
+variable "alb_certificate_arn" {}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "tags" {
+  default = {
+    Environment = "prod"
+    Application = "liveask"
+  }
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+  default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
+}
+
+variable "public_subnets" {
+  description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+  default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]
+}
+
+variable "availability_zones" {
+  description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
