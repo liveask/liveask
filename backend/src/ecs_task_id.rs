d1a25110290bf6c91fb5efe7e8ea5e04df505d75
extrawurst <mail@rusticorn.com>
2023-09-21 16:56:42 +0200

    fix ecs task id fetching

--- backend/src/ecs_task_id.rs
+++ backend/src/ecs_task_id.rs
@@ -3,17 +3,17 @@ use serde::Deserialize;
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct Response {
-    #[serde(rename(serialize = "Cluster"))]
+    #[serde(rename(deserialize = "Cluster"))]
     pub cluster: String,
-    #[serde(rename(serialize = "TaskARN"))]
+    #[serde(rename(deserialize = "TaskARN"))]
     pub task_arn: String,
-    #[serde(rename(serialize = "Revision"))]
+    #[serde(rename(deserialize = "Revision"))]
     pub revision: String,
-    #[serde(rename(serialize = "Family"))]
+    #[serde(rename(deserialize = "Family"))]
     pub family: String,
-    #[serde(rename(serialize = "DesiredStatus"))]
+    #[serde(rename(deserialize = "DesiredStatus"))]
     pub desired_status: String,
-    #[serde(rename(serialize = "KnownStatus"))]
+    #[serde(rename(deserialize = "KnownStatus"))]
     pub known_status: String,
 }
 
@@ -21,26 +21,17 @@ struct Response {
 pub async fn server_id() -> Option<String> {
     let url = std::env::var("ECS_CONTAINER_METADATA_URI_V4").ok()?;
 
-    tracing::info!(url, "[server-starting] read env",);
-
     let url = format!("{url}/task");
     let response = reqwest::Client::new().get(url).send().await.ok()?;
 
-    tracing::info!("[server-starting] fetched");
+    let parsed: Response = response.json().await.ok()?;
 
-    // let parsed: Response = response.json().await.ok()?;
-    let text = response.text().await.ok()?;
-
-    tracing::info!("[server-starting] text: {text}");
-
-    // Some(
-    //     parsed
-    //         .task_arn
-    //         .split('/')
-    //         .last()
-    //         .unwrap_or_default()
-    //         .to_string(),
-    // )
-
-    None
+    Some(
+        parsed
+            .task_arn
+            .split('/')
+            .last()
+            .unwrap_or_default()
+            .to_string(),
+    )
 }
