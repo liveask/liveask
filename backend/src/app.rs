bc074ed3e56ab755857b1f8bac734f00b2e094db
extrawurst <mail@rusticorn.com>
2023-11-07 20:54:29 +0100

    make work

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -203,30 +203,31 @@ impl App {
             // let header = poloto::header().append(Theme::light());
 
             // data.append_to(header).render_string()?
-            //
-            //
+
             let mut output = String::with_capacity(100);
 
-            let root_area = SVGBackend::with_string(&mut output, (950, 700)).into_drawing_area();
-            root_area.fill(&WHITE).unwrap();
-            let title_style = TextStyle::from(("sans-serif", 30).into_font()).color(&(BLACK));
-            root_area.titled("BEST CIRCLES", title_style).unwrap();
+            {
+                let root_area =
+                    SVGBackend::with_string(&mut output, (950, 700)).into_drawing_area();
+                root_area.fill(&WHITE).unwrap();
+                let title_style = TextStyle::from(("sans-serif", 30).into_font()).color(&(BLACK));
+                root_area.titled("BEST CIRCLES", title_style).unwrap();
 
-            let dims = root_area.dim_in_pixel();
-            let center = (dims.0 as i32 / 2, dims.1 as i32 / 2);
-            let radius = 300.0;
-            let sizes = vec![66.0, 33.0];
-            let _rgba = RGBAColor(0, 50, 255, 1.0);
-            let colors = vec![RGBColor(0, 50, 255), CYAN];
-            let labels = vec!["Pizza", "Pacman"];
+                let dims = root_area.dim_in_pixel();
+                let center = (dims.0 as i32 / 2, dims.1 as i32 / 2);
+                let radius = 300.0;
+                let sizes = vec![66.0, 33.0];
+                let colors = vec![RGBColor(0, 50, 255), CYAN];
+                let labels = vec!["Pizza", "Pacman"];
 
-            let mut pie = Pie::new(&center, &radius, &sizes, &colors, &labels);
-            pie.start_angle(66.0);
-            pie.label_style((("sans-serif", 50).into_font()).color(&(full_palette::ORANGE)));
-            pie.percentages((("sans-serif", radius * 0.08).into_font()).color(&BLACK));
-            root_area.draw(&pie).unwrap();
+                let mut pie = Pie::new(&center, &radius, &sizes, &colors, &labels);
+                pie.start_angle(66.0);
+                pie.label_style((("sans-serif", 50).into_font()).color(&(full_palette::ORANGE)));
+                pie.percentages((("sans-serif", radius * 0.08).into_font()).color(&BLACK));
+                root_area.draw(&pie).unwrap();
+            }
 
-            String::new()
+            output
         };
 
         Ok(res)
