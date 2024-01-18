ca73aa9c588d2efe88917d62309dd9196b4afa58
extrawurst <mail@rusticorn.com>
2024-01-18 18:17:22 +0100

    use serde_dynamo for contexts

--- backend/src/eventsdb/types/conversion/context.rs
+++ backend/src/eventsdb/types/conversion/context.rs
@@ -1,22 +1,15 @@
 use super::AttributeMap;
 use crate::eventsdb::Error;
 use aws_sdk_dynamodb::types::AttributeValue;
+use serde_dynamo::{from_item, to_item};
 use shared::ContextItem;
 
 pub fn contexts_to_attributes(value: Vec<ContextItem>) -> Vec<AttributeValue> {
     value
         .into_iter()
-        .map(|i| AttributeValue::M(context_item_to_attributes(i)))
+        .filter_map(|i| to_item(i).ok())
+        .map(AttributeValue::M)
         .collect()
-}
-
-fn context_item_to_attributes(value: ContextItem) -> AttributeMap {
-    let mut map = AttributeMap::new();
-
-    map.insert("label".into(), AttributeValue::S(value.label));
-    map.insert("url".into(), AttributeValue::S(value.url));
-
-    map
 }
 
 pub fn attributes_to_contexts(
@@ -38,47 +31,7 @@ pub fn attributes_to_contexts(
 }
 
 fn attributes_to_context_itemn(value: &AttributeMap) -> Result<ContextItem, super::Error> {
-    let label = value["label"]
-        .as_s()
-        .map_err(|_| Error::MalformedObject("label".into()))?
-        .clone();
+    let v = value.clone();
 
-    let url = value["url"]
-        .as_s()
-        .map_err(|_| Error::MalformedObject("url".into()))?
-        .clone();
-
-    Ok(ContextItem { label, url })
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use serde_dynamo::{from_item, to_item};
-    use shared::ContextItem;
-
-    #[test]
-    #[tracing_test::traced_test]
-    fn test_serde_dynamo_compare() {
-        let original = ContextItem {
-            label: String::from("label"),
-            url: String::from("url"),
-        };
-
-        let serde: AttributeMap = to_item(original.clone()).unwrap();
-
-        let manual = context_item_to_attributes(original.clone());
-
-        assert_eq!(serde, manual);
-
-        let from_serde: ContextItem = from_item(serde.clone()).unwrap();
-
-        assert_eq!(from_serde, original);
-
-        let from_manual = attributes_to_context_itemn(&serde).unwrap();
-
-        assert_eq!(from_manual, original);
-
-        assert_eq!(from_serde, from_manual);
-    }
+    Ok(from_item(v)?)
 }
