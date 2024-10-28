201b09fd28163b859ff4c17883a8ace4b789f1e3
extrawurst <mail@rusticorn.com>
2024-10-28 08:54:59 +0100

    cleanup

--- frontend/src/components/trusted_by.rs
+++ frontend/src/components/trusted_by.rs
@@ -4,14 +4,16 @@ use yew::{function_component, html, Html};
 pub fn TrustedBy() -> Html {
     html! {
         <>
-            <h1>{ "Trusted BY" }</h1>
             <div class="trustedby">
-                <div class="item"><img src="assets/trustedby/ms.png" /></div>
-                <div class="item"><img src="assets/trustedby/un.png" /></div>
-                <div class="item"><img src="assets/trustedby/worldbank.png" /></div>
-                <div class="item"><img src="assets/trustedby/canada.png" /></div>
-                <div class="item"><img src="assets/trustedby/lifescience.png" /></div>
-                <div class="item"><img src="assets/trustedby/alibaba.png" /></div>
+                <h2>{ "Trusted by" }</h2>
+                <div class="items">
+                    <div class="item"><img src="assets/trustedby/ms.png" /></div>
+                    <div class="item"><img src="assets/trustedby/un.svg" /></div>
+                    <div class="item"><img src="assets/trustedby/worldbank.png" /></div>
+                    <div class="item"><img src="assets/trustedby/canada.png" /></div>
+                    <div class="item"><img src="assets/trustedby/lifescience.png" /></div>
+                    <div class="item"><img src="assets/trustedby/alibaba.png" /></div>
+                </div>
             </div>
         </>
     }
