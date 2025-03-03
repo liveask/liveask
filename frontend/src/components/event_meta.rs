22f224c5c3c3adb64382edba16ba04892bd21e48
extrawurst <mail@rusticorn.com>
2025-03-03 15:23:59 +0100

    always allow changing color

--- frontend/src/components/event_meta.rs
+++ frontend/src/components/event_meta.rs
@@ -88,11 +88,11 @@ impl Component for EventMeta {
 impl EventMeta {
     fn mod_view_edit(ctx: &Context<Self>) -> Html {
         //TODO: show clock icon with tooltip that only in first 24h the text can be edited
-        let is_mod = ctx.props().tokens.is_mod() && ctx.props().is_first_24h;
+        let is_first_24h = ctx.props().tokens.is_mod() && ctx.props().is_first_24h;
         let on_click_text = ctx.link().callback(|_| Msg::EditClick);
         let on_click_color = ctx.link().callback(|_| Msg::EditColorClick);
 
-        if is_mod {
+        if is_first_24h {
             html! {
                 <>
                     <button class="button-icon" onclick={on_click_text}>
@@ -104,7 +104,11 @@ impl EventMeta {
                 </>
             }
         } else {
-            html! {}
+            html! {
+                <button class="button-icon" onclick={on_click_color}>
+                    <img src="assets/color-pick.svg" alt="edit"/>
+                </button>
+            }
         }
     }
 }
