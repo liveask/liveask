bc08b6bc1ab3ac4b646c6956491fa8f677abbac3
extrawurst <mail@rusticorn.com>
2025-12-18 16:04:01 -0500

    spinner refactor into functional component

--- frontend/src/components/spinner.rs
+++ frontend/src/components/spinner.rs
@@ -6,27 +6,18 @@ pub struct SpinnerProps {
     pub id: Option<AttrValue>,
 }
 
-pub struct Spinner;
-impl Component for Spinner {
-    type Message = ();
-    type Properties = SpinnerProps;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <div class="lds-roller" id={ctx.props().id.clone()}>
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-            </div>
-        }
+#[function_component(Spinner)]
+pub fn spinner(props: &SpinnerProps) -> Html {
+    html! {
+        <div class="lds-roller" id={props.id.clone()}>
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
     }
 }
