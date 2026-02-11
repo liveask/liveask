99414736d69888c7d5bf711a6e4e6c1a34b74ce3
extrawurst <mail@rusticorn.com>
2026-02-11 14:31:35 -0500

    version bump

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -25,7 +25,7 @@ use crate::{
     pages::{Event, Home, NewEvent, Print, Privacy},
 };
 
-pub const VERSION_STR: &str = "2.13.0";
+pub const VERSION_STR: &str = "2.13.1";
 pub const GIT_BRANCH: &str = env!("VERGEN_GIT_BRANCH");
 
 #[derive(Default, Clone, Eq, PartialEq, Store)]
